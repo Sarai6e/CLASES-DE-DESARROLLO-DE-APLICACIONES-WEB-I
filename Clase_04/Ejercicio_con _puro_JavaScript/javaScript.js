5cd25c5020b89f010b2f6a59e1a6f45bdba95c0a
Sarai6e <1472173@senati.pe>
2023-04-21 22:36:57 -0500

    ejercicios

---
+++ Clase_04/Ejercicio_con _puro_JavaScript/javaScript.js
@@ -0,0 +1,5 @@
+window.addEventListener("load",function(){
+    document.getElementById("viewAlerta").addEventListener("click",function() {
+        alert("Faltan 23 días para el día de la madre")
+    })
+})
